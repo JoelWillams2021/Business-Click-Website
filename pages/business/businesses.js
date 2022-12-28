dc87ab5b056d001896ca162bbe780a57fd9c6a71
saltywillams <70780931+JoelWillams2021@users.noreply.github.com>
2022-12-28 16:54:40 -0500

    Finished the businesses page

--- pages/business/businesses.js
+++ pages/business/businesses.js
@@ -18,37 +18,82 @@ const data = {
     ]
 }
 
-let businesses = data["businesses"];
+const table = document.getElementById("businesses_table");
 
-businesses.forEach(({name, description, tags}) => {
+const searchBar = document.getElementById("searchBar");
+const submitButton = document.getElementById("submitButton");
 
-    let outer_div = document.createElement("div");
-    outer_div.style = "flex: 1 0 21%; background-color: white; border-radius: 15px; ";
-    
-    let inner_div = document.createElement("div");
-    inner_div.classList.add("p-3");
+renderBusinesses(data["businesses"]);
 
-    let name_h4 = document.createElement("h4");
-    name_h4.innerText = name;
+// function that renders businesses on page given a list of businesses
+function renderBusinesses(business_list) {
+    table.innerHTML = ''; // clear the table
+    if(business_list.length > 0) {
+        business_list.forEach(({name, description, tags}) => {
 
-    let description_p = document.createElement("p");
-    description_p.innerText = description;
-
-    let tags_p = document.createElement("p");
-    tags_p.innerText = 'Tags: ';
-    if(tags.length > 0) {
-        tags.forEach((e) => {
-            tags_p.innerText += e;
-        })
+            let outer_div = document.createElement("div");
+            outer_div.style = "flex: 1 0 21%;";
+            outer_div.classList.add("bg-white", "rounded-4");
+            
+            let inner_div = document.createElement("div");
+            inner_div.classList.add("p-3");
+        
+            let name_h4 = document.createElement("h4");
+            name_h4.innerText = name;
+        
+            let description_p = document.createElement("p");
+            description_p.innerText = description;
+        
+            let tags_p = document.createElement("p");
+            tags_p.innerText = 'Tags: ';
+            if(tags.length > 0) {
+                tags.forEach((e) => {
+                    tags_p.innerText += e;
+                })
+            } else {
+                tags_p.innerText += 'None';
+            }
+        
+            inner_div.appendChild(name_h4);
+            inner_div.appendChild(description_p);
+            inner_div.appendChild(tags_p);
+        
+            outer_div.appendChild(inner_div);
+        
+            table.appendChild(outer_div);
+        });
     } else {
-        tags_p.innerText += 'None';
+        const message = document.createElement("p");
+        message.innerText = 'No businesses found!';
+        table.appendChild(message);
     }
+}
 
-    inner_div.appendChild(name_h4);
-    inner_div.appendChild(description_p);
-    inner_div.appendChild(tags_p);
+function changeSearchQuery(query) {
+    // when value of search bar is changed, re-render the filtered out businesses
+    let value = query.toLowerCase();
+    if(value.trim() == '') {
+        // if search bar is empty, render all businesses
+        renderBusinesses(data["businesses"]);
+    } else {
+        // filter out businesses with names that match the search query
+        let businesses = [];
+        data["businesses"].forEach((business) => {
+            if(business.name.toLowerCase() == value) {
+                businesses.push(business);
+            }
+        });
+        renderBusinesses(businesses);
+    }
+}
 
-    outer_div.appendChild(inner_div);
 
-    document.getElementById("businesses_table").appendChild(outer_div);
+searchBar.addEventListener("change", (e) => {
+    e.preventDefault();
+    changeSearchQuery(searchBar.value);
+});
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    changeSearchQuery(searchBar.value); 
 });
